193ec891cf6279591463fc69dbf740514c976705
AlexanderChum <Senneiko@yandex.ru>
2025-09-30 02:52:52 +0300

    mapper fix

--- commerce/delivery/src/main/java/ru/practicum/Mapper/DeliveryMapper.java
+++ commerce/delivery/src/main/java/ru/practicum/Mapper/DeliveryMapper.java
@@ -1,9 +1,12 @@
 package ru.practicum.Mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
+import ru.practicum.Model.Address;
 import ru.practicum.Model.Delivery;
 import ru.practicum.delivery.Models.DeliveryDto;
+import ru.practicum.warehouse.Models.AddressDto;
 
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.ERROR)
@@ -12,4 +15,9 @@ public interface DeliveryMapper {
     DeliveryDto deliveryToDto(Delivery delivery);
 
     Delivery deliveryFromDto(DeliveryDto dto);
+
+    @Mapping(target = "addressId", ignore = true)
+    Address addressFromDto(AddressDto dto);
+
+    AddressDto addressToDto(Address address);
 }
